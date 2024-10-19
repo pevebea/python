77f64aef692fab583ef31a22868040596785c250
pevebea <bea.peve99@gmail.com>
2024-10-19 15:35:34 +0000

    introduction at exemple de la syntaxe des boucles: for et while

---
+++ cours/3 boucles.py
@@ -0,0 +1,18 @@
+# LES BOUCLE EN PYTHON
+
+#boucle pour
+# for i in structure_de_donnee:
+#     instruction
+
+for i in range(0, 11, 3):
+    print(i, "bonjour")
+
+
+#boucle tantque: while
+nombre = 10
+arret = 20
+while nombre < arret:
+    print("Contenu de la boucle")
+    nombre = nombre + 1
+
+
